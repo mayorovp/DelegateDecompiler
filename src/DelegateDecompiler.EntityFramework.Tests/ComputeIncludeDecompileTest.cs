daf0b9db14425ba24fc30fa2f60c44acccd0e3e6
Pavel.Mayorov <Pavel.Mayorov@centre-it.com>
2015-06-09 13:38:32 +0500

    Add new Compute - Include - Decompile test cases

---
+++ src/DelegateDecompiler.EntityFramework.Tests/ComputeIncludeDecompileTest.cs
@@ -0,0 +1,32 @@
+﻿using DelegateDecompiler.EntityFramework.Tests.EfItems;
+using NUnit.Framework;
+using System.Data.Entity;
+using System.Linq;
+
+namespace DelegateDecompiler.EntityFramework.Tests
+{
+	[TestFixture]
+	public class ComputeIncludeDecompileTest
+	{
+		[Test]
+		public void ComputeIncludeDecompileNotFail()
+		{
+			using (var db = new EfTestDbContext())
+			{
+				db.EfParents.Where(p => ComputedSample()).Include(p => p.Children).Decompile().Load();
+			}
+		}
+
+		[Test]
+		public void ComputeDoubleIncludeDecompileNotFail()
+		{
+			using (var db = new EfTestDbContext())
+			{
+				db.EfParents.Where(p => ComputedSample()).Include(p => p.Children).Include(p => p.Children).Decompile().Load();
+			}
+		}
+
+		[Computed]
+		private static bool ComputedSample() { return true; }
+	}
+}
